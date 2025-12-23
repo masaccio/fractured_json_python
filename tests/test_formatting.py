4f9d2bdedc8abae58197262dbeea38a71765fdcf
Jon Connell <python@figsandfudge.com>
2025-12-23 16:56:49 +0000

    Fixed line lengths

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -7,7 +7,11 @@ def test_clr_load():
         '"Group2":{"Q":null,"W":[-2,-1,0,1]},'
         '"Distraction":[[],null,null]}'
     )
-    ref_output = '{\r\n  "Group1": {"X": 55, "Y": 19, "Z": -4}, \r\n  "Group2": { "Q": null, "W": [-2, -1, 0, 1] }, \r\n  "Distraction": [[], null, null]\r\n}\r\n'
+    ref_output = (
+        '{\r\n  "Group1": {"X": 55, "Y": 19, "Z": -4}, \r\n'
+        '  "Group2": { "Q": null, "W": [-2, -1, 0, 1] }, \r\n'
+        '  "Distraction": [[], null, null]\r\n}\r\n'
+    )
 
     options = FracturedJsonOptions(
         max_total_line_length=120,
